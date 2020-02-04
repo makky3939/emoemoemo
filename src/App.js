2af23b330db84d9b9bae7d40669eb12b89ec5e89
Masaki KOBAYASHI <makky.4d6b.3f5@gmail.com>
2020-02-04 17:58:26 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import Emoji from 'node-emoji';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 
-function App() {
+function EmoSpan(props) {
+  let emoString = "";
+  const emosize = props.random ? Math.floor(Math.random()*(props.emosize)+1) : props.emosize;
+
+  for (let i = 0; i < emosize; i++) {
+    emoString += Emoji.random().emoji;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <span>{emoString}</span>
+  );
+}
+
+function App() {
+  let menu = [];
+  for (let i=0; i < 6; i++) {
+    menu.push(<li key={i}><EmoSpan emosize={4} random /></li>);
+  }
+
+  let tweets = [];
+  for (let i=0; i < 200; i++) {
+    tweets.push(<p key={i}><EmoSpan emosize={30} random /></p>);
+  }
+
+  let trends = [];
+  for (let i=0; i < 10; i++) {
+    trends.push(<li key={i}><EmoSpan emosize={8} random /></li>);
+  }
+
+  return (
+    <Container>
+      <Row className="pt-4">
+        <Col md={3}>
+          <h2 className="py-3"><EmoSpan emosize={3} /></h2>
+          {menu}
+        </Col>
+        <Col md={6} className="tweets-container">
+          <h3 className="pb-3">Home</h3>
+          {tweets}
+        </Col>
+        <Col md={3}>
+          <h3 className="pb-3">Trends</h3>
+          {trends}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
